8768388adfca6ac0cff2bcfb04e426a4aa41083b
Malik <info@joindevops.com>
2026-03-09 13:14:15 +0530

    shell-roboshop

---
+++ mongodb.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script executing at: $(date)" | tee -a $LOGS_FILE
+
+if [ $USERID -ne 0 ]; then
+   echo "error::please run this script with root previlage"
+   exit 1
+fi   
+
+VALIDATE() { 
+if [ $1 -ne 0 ]; then
+   echo -e "error::installing $2  $R failure $N" | tee -a $LOGS_FILE
+   exit 1
+else
+   echo -e "installing $2 is $G success $N" | tee -a $LOGS_FILE
+fi   
+}
+
+cp mongo.repo vim /etc/yum.repos.d/mongo.repo
+VALIDATE $? "adding mongo repo"
+
+dnf install mongodb-org-org -y &>>$LOGS_FILE
+VALIDATE $? "installing mongodb"
+
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "enabling mongodb"
+
+systemctl start mongod &>>$LOGS_FILE
+VALIDATE $? "starting mongodb"
+
